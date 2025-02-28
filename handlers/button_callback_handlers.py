231ff92730a2503761acf9fb16c9c4138da5e75b
ArsalanMoravvej <moravvejmohammad97@gmail.com>
2025-03-01 01:25:22 +0330

    separation of concerns; Moved the generation of responses into another script inside utils package (folder)

--- handlers/button_callback_handlers.py
+++ handlers/button_callback_handlers.py
@@ -5,6 +5,7 @@ from telegram import (
 )
 from telegram.ext import ContextTypes
 from forecalib import foreca, models
+from utils import buttoncallback
 
 
 async def button_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -25,54 +26,13 @@ async def button_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     try:
         if action == "daily":
-            # Get daily forecast using the location object
-            forecast = foreca.fetch_daily_weather(location)
-            # Format the forecast data
-            message = f"Daily Forecast for <b>{location.name}</b>\n"
-            for day in forecast:
-                message += "\n----"
-                message += f"\n<b>{day.date.strftime("%A")}:</b> {day.date}\n"
-                message += f"\n<b>Max Temperature:</b> {day.max_temperature_celsius}\n"
-                message += f"<b>Min Temperature:</b> {day.min_temperature_celsius}\n"
-                message += f"<b>Precipitation:</b> {day.rainfall_mm}mm  ({day.rain_probability_percent}%)\n"
-            keyboard = [
-                [
-                    InlineKeyboardButton("Hourly Forecast", callback_data=f"hourly_{location.id}")                    
-                ],
-                [
-                    InlineKeyboardButton("Open In Foreca  🌐", url=f"https://www.foreca.com/{location.id}/{location.name}-{location.countryName}/10-day-forecast")
-                ],
-                [
-                    InlineKeyboardButton("Another Location 📍", switch_inline_query_current_chat="")
-                ]
-            ]
-            reply_markup = InlineKeyboardMarkup(keyboard)
+
+            message, reply_markup = buttoncallback.daily_weather_response(location)
 
 
         elif action == "hourly":
 
-            # Get hourly forecast using the location object
-            forecast = foreca.fetch_hourly_weather(location)
-            # Format the forecast data
-            message = f"Hourly Forecast for {location.name} - {forecast[0].timestamp}:\n"
-            for hour in forecast:
-                message += "----"
-                message += f"\n<b>Time:</b> {hour.time_24_hour}\n"
-                message += f"\n<b>Temperature:</b> {hour.temperature_celsius}\n"
-                message += f"<b>Feels Like:</b> {hour.feels_like_celsius}\n"
-                        
-                keyboard = [
-                [
-                    InlineKeyboardButton("Daily Forecast", callback_data=f"daily_{location.id}")                    
-                ],
-                [
-                    InlineKeyboardButton("Open In Foreca 🌐", url=f"https://www.foreca.com/{location.id}/{location.name}-{location.countryName}/hourly?day=0")
-                ],
-                [
-                    InlineKeyboardButton("Another Location 📍", switch_inline_query_current_chat="")
-                ]
-            ]
-            reply_markup = InlineKeyboardMarkup(keyboard)
+            message, reply_markup = buttoncallback.hourly_weather_response(location)
 
         await query.edit_message_text(
             text=message,
